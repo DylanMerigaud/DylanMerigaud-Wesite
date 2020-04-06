433fee8f4dfbbb30226e39659597f0579c3d96d4
DylanMerigaud <dylanmerigaud@gmail.com>
2020-04-06 11:06:24 +0200

    Add arrow first slide

--- front/src/Slides/Intro.jsx
+++ front/src/Slides/Intro.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import { makeStyles } from "@material-ui/core/styles";
+import SlideAnimation from "@material-ui/core/Slide";
+import classNames from "classnames";
 
 import Slide from "./Utils/Slide";
 
+const useStyles = makeStyles({
+  arrowTipsScroll: {
+    marginTop: "auto",
+    alignSelf: "center",
+    fontSize: 50,
+    opacity: 0,
+  },
+  arrowTipsScrollShown: {
+    opacity: 1,
+  },
+});
+
 const Intro = () => {
+  const classes = useStyles();
+  const [showArrowTipsScroll, setShowArrowTipsScroll] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowArrowTipsScroll(true);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [setShowArrowTipsScroll]);
+  console.log(showArrowTipsScroll);
   return (
     <Slide>
       <div>Intro</div>
-      <ArrowDownward />
+      <SlideAnimation direction="up" in={showArrowTipsScroll}>
+        <ArrowDownward
+          className={classNames(classes.arrowTipsScroll, {
+            [classes.arrowTipsScrollShown]: showArrowTipsScroll,
+          })}
+        />
+      </SlideAnimation>
     </Slide>
   );
 };
